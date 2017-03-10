72727f1d747538480a8e64cbf2f0d38c3fe3b558
Eugene Chiong <eugenecys@gmail.com>
2017-03-10 18:57:31 +0000

    Added trianglification as backgroundw

---
+++ js/menu.js
@@ -0,0 +1,20 @@
+---
+layout: null
+---
+$(document).ready(function () {
+
+  if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+  }
+
+  $('.btn-mobile-menu').click(function () {
+    $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+  })
+
+  $('.navigation-wrapper .blog-button').click(function () {
+    $('.navigation-wrapper').toggleClass('visible')
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+  })
+
+})
---
+++ js/render.js
@@ -0,0 +1,107 @@
+var render = (function (window) {
+
+    var SELECTORS = {
+        pattern: '.pattern',
+    };
+
+    /**
+     * Enum of CSS classes.
+     */
+    var CLASSES = {
+        patternHidden: 'pattern--hidden',
+        polygon: 'polygon',
+        polygonHidden: 'polygon--hidden'
+    };
+
+    /**
+     * Map of svg paths and points.
+     */
+    var polygonMap = {
+        paths: null,
+        points: null
+    };
+
+    /**
+     * Container of Card instances.
+     */
+    var layout = {};
+
+    /**
+     * Initialise demo.
+     */
+    
+    
+    var load = function () {
+        // For options see: https://github.com/qrohlf/Trianglify
+        var pattern = Trianglify({
+            width: window.innerWidth,
+            height: window.innerHeight,
+            cell_size: 90,
+            variance: 1,
+            stroke_width: 1,
+            x_colors: 'random',
+            y_colors: 'random'
+        }).svg(); // Render as SVG.
+
+        _mapPolygons(pattern);
+    };
+
+    /**
+     * Store path elements, map coordinates and sizes.
+     * @param {Element} pattern The SVG Element generated with Trianglify.
+     * @private
+     */
+    var _mapPolygons = function (pattern) {
+
+        // Append SVG to pattern container.
+        $(SELECTORS.pattern).append(pattern);
+
+        // Convert nodelist to array,
+        // Used `.childNodes` because IE doesn't support `.children` on SVG.
+        polygonMap.paths = [].slice.call(pattern.childNodes);
+
+        polygonMap.points = [];
+
+        polygonMap.paths.forEach(function (polygon) {
+
+            // Hide polygons by adding CSS classes to each svg path (used attrs because of IE).
+            $(polygon).attr('class', CLASSES.polygon);
+
+            var rect = polygon.getBoundingClientRect();
+
+            var point = {
+                x: rect.left + rect.width / 2,
+                y: rect.top + rect.height / 2
+            };
+
+            polygonMap.points.push(point);
+        });
+
+        // All polygons are hidden now, display the pattern container.
+        $(SELECTORS.pattern).removeClass(CLASSES.patternHidden);
+    };
+
+    /**
+     * Initialise demo.
+     */
+    
+
+    // For options see: https://github.com/qrohlf/Trianglify
+    var pattern = Trianglify({
+        width: window.innerWidth,
+        height: window.innerHeight,
+        cell_size: 90,
+        variance: 1,
+        stroke_width: 1,
+        x_colors: 'random',
+        y_colors: 'random'
+    }).svg(); // Render as SVG.
+
+    return {
+	load: load
+    };
+
+    _mapPolygons(pattern);
+})(window);
+
+window.onload = render.load;
